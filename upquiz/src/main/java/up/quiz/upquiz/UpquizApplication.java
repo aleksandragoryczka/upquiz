9c459a8903a8d2ad20767885004afcd4a726e620
aleksandragoryczka <aleksandra.goryczka@gmail.com>
2023-03-07 20:58:54 +0100

    Initial commit

---
+++ upquiz/src/main/java/up/quiz/upquiz/UpquizApplication.java
@@ -0,0 +1,14 @@
+package up.quiz.upquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpquizApplication.class, args);
+	
+	}
+
+}
---
+++ upquiz/src/test/java/up/quiz/upquiz/UpquizApplicationTests.java
@@ -0,0 +1,13 @@
+package up.quiz.upquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
